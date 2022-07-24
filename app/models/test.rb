0408658c2be1dbeef015e6d11e9eaac908e1741e
mativ <wotmatiff@yandex.ru>
2022-07-24 18:56:14 +0300

    add validations

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,6 +7,10 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   has_many :questions
 
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than: 0 }
+
   scope :easy, -> { where(level: 0..1).order(created_at: :desc) }
   scope :medium, -> { where(level: 2..4).order(created_at: :desc) }
   scope :hard, -> { where(level: 5..Float::INFINITY).order(created_at: :desc) }
