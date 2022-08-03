1cd61cec9ab1e846ed73ac03927c956bee168c22
mativ <wotmatiff@yandex.ru>
2022-08-03 20:34:57 +0300

    task 8 Done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def current_year
+    Time.zone.now.year
+  end
+
+  def github_url(author, repo)
+    link_to 'TestGuru', "https://github.com/#{author}/#{repo}", target: '_blank', rel: 'noopener'
+  end
 end
