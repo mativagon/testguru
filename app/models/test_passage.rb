a18d4cf3033130570c826f94aba29386239daff6
mativ <wotmatiff@yandex.ru>
2022-08-10 21:18:03 +0300

    add callback for setting next question

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,6 +4,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :set_next_question
 
   def completed?
     current_question.nil?
@@ -14,7 +15,6 @@ class TestPassage < ApplicationRecord
       self.correct_questions += 1
     end
 
-    self.current_question = next_question
     save!
   end
 
@@ -35,7 +35,7 @@ class TestPassage < ApplicationRecord
     current_question.answers.correct
   end
 
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+  def set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
